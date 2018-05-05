a4918239e560e4214cfaab747b42c850e9fce71a
tanhao <tanhao1210@gmail.com>
2018-05-05 19:38:59 +0800

    开代理佣金百分比细分

--- app/Application/Ag/Controller/AgentController.class.php
+++ app/Application/Ag/Controller/AgentController.class.php
@@ -1,7 +1,7 @@
 <?php
 namespace Ag\Controller;
 class AgentController extends BaseController {
-    private $rateEnum = [2,1.8,1.6,1.4,1.35,1.3,1.25,1.2,1.15,1.1,1,0.8,0.6,0.4,0.35,0.3,0.25,0.2,0.15,0.1,0.05];
+    private $rateEnum = [2,1.8,1.6,1.4,1.35,1.3,1.25,1.2,1.15,1.1,1,0.95,0.9,0.85,0.8,0.75,0.7,0.65,0.6,0.55,0.5,0.45,0.4,0.35,0.3,0.25,0.2,0.15,0.1,0.05];
     public function agentList() {
         $admin_user = M('admin_user');
         // 查询条件
